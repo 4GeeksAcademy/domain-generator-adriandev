d8a9733b80e9169501637d22b1d8814404c0137c
adriandev17 <116568010+adriandev17@users.noreply.github.com>
2025-11-01 03:23:13 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ['my', 'your', 'the'];
+  let adj = ['fast', 'code', 'web', 'new'];
+  let noun = ['studio', 'folio', 'scripter', 'cables', 'intern'];
+  let extensions = ['.com', '.io', '.er', '.es', '.net'];
+
+  let dominiosGenerados = [];
+
+  pronoun.forEach((pron) => {
+    adj.forEach((adj) => {
+      noun.forEach((noun) => {
+        
+        let nombreDeDominios = pron + adj + noun;
+
+        extensions.forEach((ext) => {
+          
+          let hack = ext.slice(1);
+          
+          if (noun.endsWith(hack)) {
+            let raiz = noun.slice(0, -hack.length);
+            
+            dominiosGenerados.push(pron + adj + raiz + ext);
+
+          } else {
+            let nombreDominiosConExt = nombreDeDominios + ext;
+
+            dominiosGenerados.push(nombreDominiosConExt);
+          }
+        });
+      });
+    });
+  });
+
+  let contenedor = document.getElementById("lista-dominios");
+
+  contenedor.innerHTML = dominiosGenerados.join("<br>");
 };
+
